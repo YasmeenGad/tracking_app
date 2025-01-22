25e4a3597b5893d16bea37b54f11f870ec8c2b45
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-22 21:56:51 +0200

    feat: impl pending orders repo

---
+++ lib/features/home/domain/contracts/pending_orders_repo.dart
@@ -0,0 +1,6 @@
+import '../../../../core/networking/common/api_result.dart';
+import '../entities/response/pending_order_response_entity.dart';
+
+abstract class PendingOrdersRepo {
+  Future<DataResult<PendingOrderResponseEntity>> getPendingOrders();
+}
