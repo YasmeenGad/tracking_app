df395f1c4dd78ce2026b63d21b00feb98b185edf
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-03 01:22:23 +0200

    fix: fix issues

---
+++ lib/features/auth/data/data_sources/impl/apply_driver_online_data_source_impl.dart
@@ -0,0 +1,39 @@
+import 'package:flowery_delivery/core/networking/api/api_manager.dart';
+import 'package:flowery_delivery/core/networking/api_execute.dart';
+import 'package:flowery_delivery/core/networking/common/api_result.dart';
+import 'package:flowery_delivery/features/auth/data/mappers/apply_driver_mapper.dart';
+import 'package:injectable/injectable.dart';
+
+import '../../../domain/entities/request/apply_request_entity.dart';
+import '../../../domain/entities/response/apply_response_entity.dart';
+import '../contracts/apply_driver_online_data_source.dart';
+
+@Injectable(as: ApplyDriverOnlineDataSource)
+class ApplyDriverOnlineDataSourceImpl implements ApplyDriverOnlineDataSource {
+  final ApiManager _apiManager;
+
+  @factoryMethod
+  ApplyDriverOnlineDataSourceImpl(this._apiManager);
+
+  @override
+  Future<DataResult<ApplyResponseEntity>> applyDriver(
+      ApplyRequestEntity request) {
+    return executeApi(() async {
+      var response = await _apiManager.applyDriver(
+          request.country!,
+          request.firstName!,
+          request.lastName!,
+          request.vehicleType!,
+          request.vehicleNumber!,
+          request.vehicleLicense!,
+          request.nID!,
+          request.nIDImg!,
+          request.email!,
+          request.password!,
+          request.rePassword!,
+          request.gender!,
+          request.phone!);
+      return ApplyDriverMapper.toEntity(response);
+    });
+  }
+}
