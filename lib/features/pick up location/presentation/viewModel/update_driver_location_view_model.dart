f182985b647fa7ef71f530b2ebfd07bd4d3d0c21
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-05 03:03:32 +0200

    refactor: refactor location view

---
+++ lib/features/pick up location/presentation/viewModel/update_driver_location_view_model.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import '../../../order_details/data/models/order_details_model.dart';
+import '../../../order_details/presentation/viewModel/order_details_actions.dart';
+import '../../../order_details/presentation/viewModel/order_details_view_model_cubit.dart';
+import '../../data/models/address_details_model.dart';
+
+class UpdateDriverLocationViewModel with ChangeNotifier {
+  // <-- Add ChangeNotifier
+  final OrderDetailsViewModelCubit viewModel;
+  LocationData? currentLocation;
+  final Location location = Location();
+
+  UpdateDriverLocationViewModel(this.viewModel);
+
+  Future<void> getCurrentLocation(
+      AddressDetailsModel? addressDetailsModel) async {
+    try {
+      var userLocation = await location.getLocation();
+      currentLocation = userLocation;
+      notifyListeners(); // <-- Notify UI to rebuild
+
+      _updateLocation(addressDetailsModel, userLocation);
+
+      location.onLocationChanged.listen((LocationData newLocation) {
+        currentLocation = newLocation;
+        notifyListeners(); // <-- Notify UI to rebuild on location change
+        _updateLocation(addressDetailsModel, newLocation);
+      });
+    } catch (e) {
+      currentLocation = null;
+      notifyListeners();
+    }
+  }
+
+  void _updateLocation(
+      AddressDetailsModel? addressDetailsModel, LocationData locationData) {
+    if (addressDetailsModel != null) {
+      viewModel.doAction(UpdateLocation(
+        userId: addressDetailsModel.userId,
+        orderId: addressDetailsModel.orderId,
+        location: LocationModel(
+          latitude: locationData.latitude,
+          longitude: locationData.longitude,
+        ),
+      ));
+    }
+  }
+}
--- lib/features/pick up location/presentation/views/location_view.dart
+++ lib/features/pick up location/presentation/views/location_view.dart
@@ -1,156 +1,122 @@
-import 'package:flowery_delivery/core/styles/colors/my_colors.dart';
-import 'package:flowery_delivery/di/di.dart';
-import 'package:flowery_delivery/features/order_details/data/models/order_details_model.dart';
-import 'package:flowery_delivery/features/order_details/presentation/viewModel/order_details_actions.dart';
-import 'package:flowery_delivery/features/pick%20up%20location/presentation/widgets/delivery_location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:provider/provider.dart';
 import 'package:location/location.dart';
-
+import 'package:flowery_delivery/core/styles/colors/my_colors.dart';
+import 'package:flowery_delivery/di/di.dart';
+import 'package:flowery_delivery/features/order_details/presentation/viewModel/order_details_view_model_cubit.dart';
+import 'package:flowery_delivery/features/pick%20up%20location/presentation/widgets/delivery_location.dart';
 import '../../../../core/utils/widgets/base/snack_bar.dart';
-import '../../../order_details/presentation/viewModel/order_details_view_model_cubit.dart';
 import '../../data/models/address_details_model.dart';
+import '../viewModel/update_driver_location_view_model.dart';
 import '../widgets/custom_address_details.dart';
 
 class LocationView extends StatefulWidget {
   final AddressDetailsModel? addressDetailsModel;
 
-  const LocationView({
-    super.key,
-    this.addressDetailsModel,
-  });
+  const LocationView({super.key, this.addressDetailsModel});
 
   @override
   _LocationViewState createState() => _LocationViewState();
 }
 
 class _LocationViewState extends State<LocationView> {
-  LocationData? currentLocation;
   final MapController mapController = MapController();
   late final OrderDetailsViewModelCubit viewModel;
+  late final UpdateDriverLocationViewModel locationViewModel;
 
   @override
   void initState() {
     super.initState();
-    _getCurrentLocation();
     viewModel = getIt<OrderDetailsViewModelCubit>();
-  }
-
-  Future<void> _getCurrentLocation() async {
-    var location = Location();
-
-    try {
-      var userLocation = await location.getLocation();
-      setState(() {
-        currentLocation = userLocation;
-      });
-      viewModel.doAction(UpdateLocation(
-        userId: widget.addressDetailsModel!.userId,
-        orderId: widget.addressDetailsModel!.orderId,
-        location: LocationModel(
-          latitude: userLocation.latitude,
-          longitude: userLocation.longitude,
-        ),
-      ));
-    } on Exception {
-      currentLocation = null;
-    }
-
-    location.onLocationChanged.listen((LocationData newLocation) {
-      setState(() {
-        currentLocation = newLocation;
-      });
-      viewModel.doAction(UpdateLocation(
-        userId: widget.addressDetailsModel!.userId,
-        orderId: widget.addressDetailsModel!.orderId,
-        location: LocationModel(
-          latitude: newLocation.latitude,
-          longitude: newLocation.longitude,
-        ),
-      ));
-    });
+    locationViewModel = UpdateDriverLocationViewModel(viewModel);
+    locationViewModel.getCurrentLocation(widget.addressDetailsModel);
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => viewModel,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => locationViewModel),
+        BlocProvider(create: (context) => viewModel),
+      ],
       child: Stack(
         children: [
           BlocListener<OrderDetailsViewModelCubit, OrderDetailsViewModelState>(
             listener: (context, state) {
-              switch (state) {
-                case UpdateLocationSuccess():
-                  debugPrint("location updated");
-                case UpdateLocationError():
-                  debugPrint(state.errorMessage);
-                  return aweSnackBar(
-                    context: context,
-                    title: "Error",
-                    msg: state.errorMessage,
-                    type: MessageTypeConst.failure,
-                  );
-
-                default:
-                  null;
+              if (state is UpdateLocationSuccess) {
+                debugPrint("location updated");
+              } else if (state is UpdateLocationError) {
+                debugPrint(state.errorMessage);
+                aweSnackBar(
+                  context: context,
+                  title: "Error",
+                  msg: state.errorMessage,
+                  type: MessageTypeConst.failure,
+                );
               }
             },
-            child: Column(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: currentLocation == null
-                      ? Center(
-                          child: Center(
+            child: Consumer<UpdateDriverLocationViewModel>(
+              builder: (context, state, child) {
+                return Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: state.currentLocation == null
+                          ? Center(
                               child: SpinKitThreeInOut(
-                          color: MyColors.baseColor,
+                                color: MyColors.baseColor,
                           size: 40.0,
-                        )))
-                      : FlutterMap(
-                          mapController: mapController,
-                          options: MapOptions(
-                            initialCenter: LatLng(currentLocation!.latitude!,
-                                currentLocation!.longitude!),
-                            initialZoom: 15.0,
-                            onTap: (tapPosition, point) {
-                              debugPrint("Tapped at: $point");
-                            },
-                          ),
-                          children: [
-                            TileLayer(
-                              urlTemplate:
-                                  "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                              subdomains: const ['a', 'b', 'c'],
-                            ),
-                            MarkerLayer(
-                              markers: [
-                                if (currentLocation != null)
-                                  Marker(
+                              ),
+                            )
+                          : FlutterMap(
+                              mapController: mapController,
+                              options: MapOptions(
+                                initialCenter: LatLng(
+                                  state.currentLocation!.latitude!,
+                                  state.currentLocation!.longitude!,
+                                ),
+                                initialZoom: 15.0,
+                              ),
+                              children: [
+                                TileLayer(
+                                  urlTemplate:
+                                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                                  subdomains: const ['a', 'b', 'c'],
+                                ),
+                                MarkerLayer(
+                                  markers: [
+                                    Marker(
                                       width: 115,
                                       height: 35,
-                                      point: LatLng(currentLocation!.latitude!,
-                                          currentLocation!.longitude!),
-                                      child: const DeliveryLocation()),
+                                      point: LatLng(
+                                        state.currentLocation!.latitude!,
+                                        state.currentLocation!.longitude!,
+                                      ),
+                                      child: const DeliveryLocation(),
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
-                          ],
-                        ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: CustomAddressDetails(
-                    addressDetailsModel: widget.addressDetailsModel,
-                  ),
-                ),
-              ],
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: CustomAddressDetails(
+                        addressDetailsModel: widget.addressDetailsModel,
+                      ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
           Positioned(
-            top: 40.h, // Adjust for padding
+            top: 40.h,
             left: 16.w,
             child: GestureDetector(
               onTap: () => Navigator.of(context).pop(),
